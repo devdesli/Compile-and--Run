8f75e153254371a244550c8c88918c2fa0a82981
develloperdesli <develloperdesli@gmail.com>
2025-06-05 18:27:54 +0200

    setup webvieuw

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This is the README for your extension "c-and-r". After writing up a brief description, we recommend including the following sections.
 
+type compile and run or car to run extension.
 ## Features
 
 Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
--- extension.js
+++ extension.js
@@ -1,25 +1,52 @@
+const vscode = require("vscode");
 
-const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
+
 function activate(context) {
-    // runned once when the extension is activated
-	console.log('Congratulations, your extension "c-and-r" is now active!');
+  // runned once when the command is activated
+  const panel = vscode.window.createWebviewPanel(
+    "carWebview", // Identifies the type of the webview. Used internally
+    "Welcome to CAR", // Title of the panel displayed to the user
+    vscode.ViewColumn.One, // Editor column to show the new webview panel in
+    {} // Webview options
+  );
 
-	// The command has been defined in the package.json file
-	let disposable = vscode.commands.registerCommand
-	('c-and-r.helloWorld', function () {
-		vscode.window.showInformationMessage('Hello World from c_and_r!');
-	});
+  panel.webview.html = `
+	<html>
+	<style>
+	  body {
+	    font-family: Arial, sans-serif;
+	    margin: 20px;
+	    background-color: #f0f0f0;
+	  }
+		h1 {
+		color: #000000;
+      }
+	  p {
+	  color: #000000;
+	  }
+	</style>
+	  <body>
+		<h1>Welcome to Car Webview!</h1>
+		<p>Define input and output to be set to car in package.json</p>
+	  </body>
+	</html>`;
+	
+  // The command has been defined in the package.json file
+  // and will run every time its typed
+  let disposable = vscode.commands.registerCommand("c-and-r.car", function () {
+    vscode.window.showInformationMessage("Car command executed!");
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
